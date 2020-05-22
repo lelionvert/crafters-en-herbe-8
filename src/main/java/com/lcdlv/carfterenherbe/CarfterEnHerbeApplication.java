efe2fc4cf52247a6db533d5b902b4a80c4519458
NOVEMBRE Manuel <novembremanuel@gmail.com>
2020-05-22 18:18:35 +0200

    project set up

---
+++ src/main/java/com/lcdlv/carfterenherbe/CarfterEnHerbeApplication.java
@@ -0,0 +1,13 @@
+package com.lcdlv.carfterenherbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarfterEnHerbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarfterEnHerbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lcdlv/carfterenherbe/CarfterEnHerbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcdlv.carfterenherbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarfterEnHerbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
